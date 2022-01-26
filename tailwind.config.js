6a5465fcab5fc2c6812f7807c7852d226d2adfd5
Thomas Poirier <thpoirier@gmail.com>
2022-01-26 20:00:14 +0100

    lets go

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,32 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Barlow Semi Condensed'],
+      barlow: ['Barlow'],
+      caveat: ['Caveat'],
+      abahia: ['Abhaya Libre'],
+      bodoni: ['Bodoni Moda'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: '#D18D57', // brown-contact
+          100: '#B5CAD7', // bleu-gris
+          200: '#54ABBC', // bg-exergue
+          300: '#5FB1C0', // bg-jurisdictions
+          400: '#379EB1', // bg-activity
+          500: '#4493B2', // bg-h
+          600: '#40748A', // text-bit-lighter (passion du droit)
+          700: '#3B5D69', // text-strong (actus)
+          800: '#1F4D5E', // menu
+          900: '#3B5D69', // bg btn & footer
+        },
+      },
+    },
   },
   plugins: [],
 }
